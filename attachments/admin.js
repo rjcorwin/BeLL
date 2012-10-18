9240699185b955a080626470d7f6862a1dc3fc26
rjsteinert <rj@rjsteinert.com>
2012-10-18 13:41:40 -0400

    Added a #page-resource-form page. Not working yet, just copied from @rwadholm's The Little Library project.

--- attachments/admin.js
+++ attachments/admin.js
@@ -27,3 +27,203 @@ function deleteAllResources() {
     }
   })
 }
+
+
+jQuery.fn.saveResourceForm = function(itemID, itemType) {
+  
+  // Get all of the values from the form fields
+  var itemTitle = $('.settingsForm input#title').val(),
+    itemAuthor = $('.settingsForm input#author').val(),
+    itemDescription = $('.settingsForm textarea#description').val(),
+    itemDate = $('.settingsForm input#date').val(),
+    itemRev = $('.settingsForm input#_rev').val(),
+    itemDelete = $('.settingsForm input#delete:checked').val(),
+    itemType = $('.settingsForm select').val(),
+    itemFilename = $('.settingsForm input:file').val(); 
+  
+  // Check for new uploaded file
+  if (itemFilename == undefined || itemFilename == ""){
+    $('.settingsForm input:file').remove();
+    itemFilename = "";
+  }
+  else {
+    itemFilename = itemFilename.replace(/^C:\\fakepath\\/i, '');
+  }
+  
+  
+   // If no new file, then fall back on the old filename
+  if (!itemFilename || itemFilename.length == 0) {
+    itemFilename = $('.settingsForm input#filename').val();
+  }
+  
+  // Force to add a title (the only required field)
+  if (!itemTitle || itemTitle.length == 0) {
+    alert(libLang.addTitle); // Get text for language
+    return;
+  }
+  
+  // Check if size of db is above the limit
+  dbSize = maxDBSize;
+  $.ajax({
+    url: "/"+ homeURL,
+    dataType: 'json',
+    async: false,
+    success: function(dbInfo){
+      dbSize = dbInfo.data_size;
+    }
+  });
+  if (itemDelete != 'Yes' && dbSize >= maxDBSize){
+    alert(libLang.noSpace);
+    return;
+  }
+  
+  
+  
+  /*
+  // Requires an uploaded file
+  if (!revData._attachments || revData._attachments.length == 0) {
+    alert("Please select a file to upload.");
+    return;
+  }*/
+  
+  
+  
+  
+  if (itemDelete != 'Yes'){
+  
+    if (itemID != 'add'){
+      
+      // Update existing record
+      $(this).ajaxSubmit({
+        url: "/"+ homeURL +"/"+ itemID,
+        data: {"filename":itemFilename},
+        success: function(resp) {
+        
+          $.getJSON("/"+ homeURL +"/"+ itemID, function(revData) {
+            itemRev = revData._rev;
+            itemAttachment = revData._attachments;
+            user = revData.user;
+            
+            if (!revData._attachments || revData._attachments.length == 0) {
+              
+              $.couch.db(homeURL).saveDoc({
+                "_id": itemID,
+                "_rev": itemRev,
+                "filename":itemFilename,
+                "title":itemTitle,
+                "author":itemAuthor,
+                "type":itemType,
+                "description":itemDescription,
+                "date":itemDate,
+                "user":user
+              }, {
+                success: function() { 
+                  alert(libLang.saved); // Get text for language
+                  window.location.replace("index.html");
+                }
+              });
+            }
+            else {
+              $.couch.db(homeURL).saveDoc({
+                "_id": itemID,
+                "_rev": itemRev,
+                "filename":itemFilename,
+                "title":itemTitle,
+                "author":itemAuthor,
+                "type":itemType,
+                "description":itemDescription,
+                "date":itemDate,
+                "user":user,
+                "_attachments":itemAttachment
+              }, {
+                success: function() { 
+                  alert(libLang.saved); // Get text for language
+                  window.location.replace("index.html");
+                }
+              });
+            };
+          });
+        }
+      });
+    } 
+    else {
+      
+      
+      // Add new record
+      uniqueID = $.couch.newUUID();
+      itemID = itemTitle.replace(/[\s]/g,'_');
+      itemID = homeUser +'-'+ itemType.charAt(0).toUpperCase() + itemType.slice(1) +'-'+  encodeURI(itemID) +'-'+ uniqueID;
+      itemID = itemID.replace(/[^a-z 0-9 _ -]+/gi,'');
+      
+      
+      $('form .settingsForm').attr({"action":"/"+ homeURL +"/"+ itemID});
+      
+      // Save information
+      $.couch.db(homeURL).saveDoc({
+        "_id": itemID,
+        "filename":itemFilename,
+        "title":itemTitle,
+        "author":itemAuthor,
+        "type":itemType,
+        "description":itemDescription,
+        "date":itemDate,
+        "user":homeUser
+      }, {
+        success: function(){
+          
+          // Get saved info, then add attachment to item
+          $.getJSON("/"+ homeURL +"/"+ itemID, function(revData) {
+            
+            $('.settingsForm input#_rev').val(revData._rev);
+            
+            var data = {};
+
+            $.each($("form :input").serializeArray(), function(i, field) {
+              data[field.name] = field.value;
+            });
+            
+            $("form :file").each(function() {
+              data[this.name] = this.value.replace(/^C:\\fakepath\\/g, ''); // file inputs need special handling
+            });
+            
+            itemFilename = data._attachments;
+              
+            
+            $('form.settingsForm').ajaxSubmit({
+              url: "/"+ homeURL +"/"+ itemID,
+              success: function(resp) {
+                $.getJSON("/"+ homeURL +"/"+ itemID, function(saveData) {
+                  itemRev = saveData._rev;
+                  itemAttachment = saveData._attachments;
+                  
+                  // Resave all information
+                  $.couch.db(homeURL).saveDoc({
+                    "_id": itemID,
+                    "_rev": itemRev,
+                    "filename":itemFilename,
+                    "title":itemTitle,
+                    "author":itemAuthor,
+                    "type":itemType,
+                    "description":itemDescription,
+                    "date":itemDate,
+                    "user":homeUser,
+                    "_attachments":itemAttachment
+                  }, {
+                    success: function() { 
+                      alert(libLang.saved); // Get text for language
+                      window.location.replace("index.html");
+                    }
+                  });
+                });
+              }
+            });
+          });
+        }
+      });     
+    };      
+  } else {
+    // Delete the item from the library
+    $.couch.db(homeURL).removeDoc({'_id': itemID, "_rev": itemRev});
+    window.location.replace("index.html");
+  } 
+};
--- attachments/my.js
+++ attachments/my.js
@@ -74,6 +74,89 @@
   })
 
 
+  // Initialize the page
+  $("#page-resource-form").live("pagebeforeshow", function(e, d) {
+
+
+    // Get text for language
+    $('.deleteLabel span.ui-btn-text').text(libLang.deleteLabel);
+    $('.titleLabel').text(libLang.titleLabel);
+    $('.authorLabel').text(libLang.authorLabel);
+    $('.descriptionLabel').text(libLang.descriptionLabel);
+    $('.dateLabel').text(libLang.dateLabel);
+    $('.typeLabel').text(libLang.typeLabel);
+    $('.fileLabel').text(libLang.fileLabel);
+    $('.finalField .ui-btn-text').text(libLang.submitBtn);
+    $('.Submit').val(libLang.submitBtn);
+    $('.select-books').text(libLang.books);
+    $('.select-pictures').text(libLang.pictures);
+    $('.select-videos').text(libLang.videos);
+    $('.select-audio').text(libLang.audio);
+    $('.select-documents').text(libLang.documents);
+    $('.select-notes').text(libLang.notes);
+    $('.select-courses').text(libLang.courses);
+    
+    // Get the document id and type (i.e. book, video, etc.) from the URL
+    var itemID = $('#settings').getParameter('id'),
+      itemType = $('#settings').getParameter('type');
+    
+    // Set the title of the page
+    $('h1').text(libLang.add +' '+ libLang[itemType]);
+    
+    // Set the item's type/category
+    $('.settingsForm .typeField .ui-btn-text').text(libLang[itemType]);
+    $('.settingsForm select#type').val(itemType);
+    
+    // If user has clicked on add button, don't look for any JSON files to fill in form and remove delete button
+    $('.deleteButton').fadeOut();
+    
+    if (itemType == 'notes'){
+      
+      $('.settingsForm .descriptionLabel').text(libLang[itemType]);
+      $('.settingsForm #description').attr('style','height: 250px;');
+    }
+    
+    // Fill out the form with data if it exists
+    if (itemID != 'add'){
+      $('.deleteButton').fadeIn();
+      $('.settingsForm').formShow(itemID);
+    };
+    
+    
+    /*// Show progress of uploads
+    $('input[type="file"]').change(function(){
+      $(this).after('<progress max="100" value="50">50</progress>');
+    });*/
+    
+    // When user clicks on the form submit button, information is saved to the database
+    $('input.Submit').click(function(event){
+      
+        event.preventDefault();
+      
+      $('.settingsForm').sendForm(itemID, itemType);
+    
+    });
+    
+    // Allow users to delete single file attachments to the item
+    $('a.deleteThisFile').live('click', function(event){
+      
+      event.preventDefault();
+      
+      fileURL = $(this).attr('rel');
+      
+      $(this).parent().fadeOut();
+      
+      $.getJSON("/"+ homeURL +"/"+ itemID, function(revData) {
+        itemRev = revData._rev;
+        $.ajax({
+          url: '/'+ homeURL +'/'+ itemID +'/'+ fileURL +'?rev='+ itemRev,
+          dataType: 'json',
+          type: 'DELETE'
+        });
+      });
+    });
+  });
+
   /* 
    * Page: page-which-level
    */
